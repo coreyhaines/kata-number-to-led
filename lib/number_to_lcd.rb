600f27e3a8bcd04026a4bf58f7f627868aa06fd0
Corey Haines <coreyhaines@gmail.com>
2009-10-04 14:07:01 -0500

    refactoring

--- lib/number_to_lcd.rb
+++ lib/number_to_lcd.rb
@@ -1,34 +1,76 @@
 class NumberToLcd
   class << self
-
-    LINES_FOR = {
-      1 => [:blank, :right, :blank, :right, :blank],
-      2 => [:middle, :right, :middle, :left, :middle],
-      3 => [:middle, :right, :middle, :right, :middle],
-      4 => [:blank, :left_and_right, :middle, :right, :blank],
-      5 => [:middle, :left, :middle, :right, :middle],
-      6 => [:middle, :left, :middle, :left_and_right, :middle],
-      7 => [:middle, :right, :blank, :right, :blank],
-      8 => [:middle, :left_and_right, :middle, :left_and_right, :middle],
-      9 => [:middle, :left_and_right, :middle, :right, :middle],
-      0 => [:middle, :left_and_right, :blank, :left_and_right, :middle]
-    }
     def convert(this_number)
       lines = LINES_FOR[this_number]
       lines_to_strings(lines).join("\n")
     end
-    
+
     def lines_to_strings(lines)
       lines.map do |line_type|
         STRINGS_FOR_TYPE[line_type]
       end
     end
 
-    STRINGS_FOR_TYPE = { :blank          => "   ",
-      :left           => "|  ",
-      :right          => "  |",
-      :left_and_right => "| |",
-      :middle         => " - "
-    }
   end
+
+  STRINGS_FOR_TYPE = {
+    :blank          => "   ",
+    :left           => "|  ",
+    :right          => "  |",
+    :left_and_right => "| |",
+    :middle         => " - "
+  }
+
+  LINES_FOR = {
+    1 => [:blank,
+          :right,
+          :blank,
+          :right,
+          :blank],
+    2 => [:middle,
+          :right,
+          :middle,
+          :left,
+          :middle],
+    3 => [:middle,
+          :right,
+          :middle,
+          :right,
+          :middle],
+    4 => [:blank,
+          :left_and_right,
+          :middle,
+          :right,
+          :blank],
+    5 => [:middle,
+          :left,
+          :middle,
+          :right,
+          :middle],
+    6 => [:middle,
+          :left,
+          :middle,
+          :left_and_right,
+          :middle],
+    7 => [:middle,
+          :right,
+          :blank,
+          :right,
+          :blank],
+    8 => [:middle,
+          :left_and_right,
+          :middle,
+          :left_and_right,
+          :middle],
+    9 => [:middle,
+          :left_and_right,
+          :middle,
+          :right,
+          :middle],
+    0 => [:middle,
+          :left_and_right,
+          :blank,
+          :left_and_right,
+          :middle]
+  }
 end
