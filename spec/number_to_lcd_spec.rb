4cfe15be0bd0c5e2f42a1878d5da58528325e333
Corey Haines <coreyhaines@gmail.com>
2009-10-04 15:20:26 -0500

    custom matcher for spc

--- spec/number_to_lcd_spec.rb
+++ spec/number_to_lcd_spec.rb
@@ -1,38 +1,53 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+Spec::Matchers.define :convert_to do |expected|
+  match do |actual|
+    NumberToLcd.convert(actual) == expected.chop
+  end
+  failure_message_for_should do |actual|
+    "Expected #{actual} to be\n#{expected.chop}\nbut was\n#{NumberToLcd.convert(actual)}"
+  end
+end
 describe NumberToLcd do
-  context "no scaling" do
-    context "single digits" do
-      it "convert 1" do
-        NumberToLcd.convert(1).should == "   \n  |\n   \n  |\n   "
-      end
-      it "converts 2" do
-        NumberToLcd.convert(2).should == " - \n  |\n - \n|  \n - "
-      end
-      it "converts 3" do
-        NumberToLcd.convert(3).should == " - \n  |\n - \n  |\n - "
-      end
-      it "converts 4" do
-        NumberToLcd.convert(4).should == "   \n| |\n - \n  |\n   "
-      end
-      it "converts 5" do
-        NumberToLcd.convert(5).should == " - \n|  \n - \n  |\n - "
-      end
-      it "converts 6" do
-        NumberToLcd.convert(6).should == " - \n|  \n - \n| |\n - "
-      end
-      it "converts 7" do
-        NumberToLcd.convert(7).should == " - \n  |\n   \n  |\n   "
-      end
-      it "converts 8" do
-        NumberToLcd.convert(8).should == " - \n| |\n - \n| |\n - "
-      end
-      it "converts 9" do
-        NumberToLcd.convert(9).should == " - \n| |\n - \n  |\n - "
-      end
-      it "converts 0" do
-        NumberToLcd.convert(0).should == " - \n| |\n   \n| |\n - "
-      end
+  def check_conversion(number, lcd)
+  end
+  context "single digits" do
+    it "converts 1" do
+      expected = <<EOF
+   
+  |
+   
+  |
+   
+EOF
+      1.should convert_to(expected)
+    end
+    it "converts 2" do
+      NumberToLcd.convert(2).should == " - \n  |\n - \n|  \n - "
+    end
+    it "converts 3" do
+      NumberToLcd.convert(3).should == " - \n  |\n - \n  |\n - "
+    end
+    it "converts 4" do
+      NumberToLcd.convert(4).should == "   \n| |\n - \n  |\n   "
+    end
+    it "converts 5" do
+      NumberToLcd.convert(5).should == " - \n|  \n - \n  |\n - "
+    end
+    it "converts 6" do
+      NumberToLcd.convert(6).should == " - \n|  \n - \n| |\n - "
+    end
+    it "converts 7" do
+      NumberToLcd.convert(7).should == " - \n  |\n   \n  |\n   "
+    end
+    it "converts 8" do
+      NumberToLcd.convert(8).should == " - \n| |\n - \n| |\n - "
+    end
+    it "converts 9" do
+      NumberToLcd.convert(9).should == " - \n| |\n - \n  |\n - "
+    end
+    it "converts 0" do
+      NumberToLcd.convert(0).should == " - \n| |\n   \n| |\n - "
     end
   end
 
