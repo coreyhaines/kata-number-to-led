230690429e8c9caf101fc1b84f8ac303259268d3
Corey Haines <coreyhaines@gmail.com>
2009-10-04 13:46:57 -0500

    list of specs

--- spec/number_to_lcd_spec.rb
+++ spec/number_to_lcd_spec.rb
@@ -1,7 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe NumberToLcd do
-  it "can convert a number" do
-    NumberToLcd.convert(1)
+  context "single digits" do
+    it "converts 1"
+    it "converts 2"
+    it "converts 3"
+    it "converts 4"
+    it "converts 5"
+    it "converts 6"
+    it "converts 7"
+    it "converts 8"
+    it "converts 9"
+    it "converts 0"
   end
 end
