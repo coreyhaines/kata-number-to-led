92763daab0870c20007c631af7c43471aaa01490
Corey Haines <coreyhaines@gmail.com>
2009-10-04 12:36:57 -0500

    1 and 2

--- lib/number_to_lcd.rb
+++ lib/number_to_lcd.rb
@@ -1,4 +1,19 @@
 class NumberToLcd
-  def self.convert(this_number)
+  class << self
+    def convert(this_number)
+      return "#{blank}\n#{right}\n#{blank}\n#{right}\n#{blank}\n" if this_number == 1
+      return "#{middle}\n#{right}\n#{middle}\n#{left}\n#{middle}\n" if this_number == 2
+    end
+    
+    { :blank  => "   ",
+      :left   => "|  ",
+      :right  => "  |",
+      :middle => " - "
+    }.each_pair do |key, string|
+      define_method(key) do
+        string
+      end
+    end
+    
   end
 end
--- spec/number_to_lcd_spec.rb
+++ spec/number_to_lcd_spec.rb
@@ -1,7 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe NumberToLcd do
-  it "can convert a number" do
-    NumberToLcd.convert(1)
+  context "no scaling" do
+    context "single digits" do
+      it "convert 1" do
+        NumberToLcd.convert(1).should == "   \n  |\n   \n  |\n   \n"
+      end
+      it "converts 2" do
+        NumberToLcd.convert(2).should == " - \n  |\n - \n|  \n - \n"
+      end
+    end
   end
 end
