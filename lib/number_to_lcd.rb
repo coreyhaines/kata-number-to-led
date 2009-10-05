68ba461fe1ba1855680b3dddac93823a955af023
Corey Haines <coreyhaines@gmail.com>
2009-10-04 19:49:02 -0500

    keying with strings

--- lib/number_to_lcd.rb
+++ lib/number_to_lcd.rb
@@ -1,19 +1,21 @@
 class NumberToLcd
   class << self
     def convert(this_number)
-      lines = LINES_FOR[this_number]
+      lines = lines_for(this_number)
       lines_to_strings(lines).join("\n")
     end
 
+    def lines_for(this_number)
+      LINES_FOR[this_number.to_s]
+    end
     def lines_to_strings(lines)
       lines.map do |line_type|
-        STRINGS_FOR_TYPE[line_type]
+        STRING_FOR_LINE_TYPE[line_type]
       end
     end
-
   end
 
-  STRINGS_FOR_TYPE = {
+  STRING_FOR_LINE_TYPE = {
     :blank          => "   ",
     :left           => "|  ",
     :right          => "  |",
@@ -22,52 +24,52 @@ class NumberToLcd
   }
 
   LINES_FOR = {
-    1 => [:blank,
+    '1' => [:blank,
           :right,
           :blank,
           :right,
           :blank],
-    2 => [:middle,
+    '2' => [:middle,
           :right,
           :middle,
           :left,
           :middle],
-    3 => [:middle,
+    '3' => [:middle,
           :right,
           :middle,
           :right,
           :middle],
-    4 => [:blank,
+    '4' => [:blank,
           :left_and_right,
           :middle,
           :right,
           :blank],
-    5 => [:middle,
+    '5' => [:middle,
           :left,
           :middle,
           :right,
           :middle],
-    6 => [:middle,
+    '6' => [:middle,
           :left,
           :middle,
           :left_and_right,
           :middle],
-    7 => [:middle,
+    '7' => [:middle,
           :right,
           :blank,
           :right,
           :blank],
-    8 => [:middle,
+    '8' => [:middle,
           :left_and_right,
           :middle,
           :left_and_right,
           :middle],
-    9 => [:middle,
+    '9' => [:middle,
           :left_and_right,
           :middle,
           :right,
           :middle],
-    0 => [:middle,
+    '0' => [:middle,
           :left_and_right,
           :blank,
           :left_and_right,
